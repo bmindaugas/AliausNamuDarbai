55317308d1b6e6e428ceebc6f98e595df941b9fb
bmindaugas <37580537+bmindaugas@users.noreply.github.com>
2018-07-20 12:50:39 +0300

    c

--- LoterijaAtspekSkaiciu/src/com/company/Main.java
+++ LoterijaAtspekSkaiciu/src/com/company/Main.java
@@ -1,7 +1,11 @@
 package com.company;
+import jdk.nashorn.api.tree.ContinueTree;
+
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 import static java.lang.System.in;
+import static java.lang.System.setOut;
 
 public class Main {
 
@@ -15,36 +19,55 @@ public class Main {
     Scanner vardas = new Scanner(in);
     tavoVardas = vardas.nextLine();
 
-
-    System.out.println("Labas "+tavoVardas);
+    System.out.println("Labas " + tavoVardas.toUpperCase());
     System.out.println("Kiek tau metu? ");
 
     int kiekMetu;
     Scanner metai = new Scanner(in);
     kiekMetu = metai.nextInt();
 
+    if (kiekMetu < 18) {
+      System.out.println("TU NEPILNAMETIS IR NEGALI ZAISTI AZARTINIU ZAIDIMU");
+      return;
+    } else {
+      System.out.println(tavoVardas.toUpperCase() + " tau reikes atspeti skaiciu nuo nulio iki desimt");
+    }
+
     Scanner spejimuKiekiui = new Scanner(in);
     int kiekKartu;
     System.out.println("Is kiek kartu galvoji, kad atspesi?");
     kiekKartu = spejimuKiekiui.nextInt();
+    if (kiekKartu < 0 || kiekKartu > 10) {
+      System.out.println(tavoVardas.toUpperCase() + " ZIUREK KA VEDI.");
+      System.out.println("Gerai bandyk dar karta");
+      return;
+    } else {
 
-    while (kiekKartu>0){
+      while (kiekKartu > 0) {
 
-      System.out.println("Atspek skaiciu nuo 0 iki 10 ");
+        System.out.println("Gerai spek jau sugalvojau.");
 
-      int r = (int)(Math.random()*10);
-      Scanner klavetura = new Scanner(in);
-      int skaicius = klavetura.nextInt();
-      if (skaicius == r){
-        System.out.println("Sveikinam!!!");
-        System.out.println("------------");
-        break;
+        int r = (int) (Math.random() * 3);
+        Scanner klavetura = new Scanner(in);
+        int skaicius = klavetura.nextInt();
+        if (skaicius == r) {
+          System.out.println("++++++++++++");
+          System.out.println("------------");
+          System.out.println("++++++++++++");
+
+          System.out.println(tavoVardas.toUpperCase());
+          System.out.println();
+          System.out.println("SVEIKINAM TAVE!!!");
+          System.out.println("++++++++++++");
+          System.out.println("------------");
+          System.out.println("++++++++++++");
+          break;
+        } else {
+          System.out.println("Bandykite dar karta :(");
+          System.out.println(r);
+        }
+        kiekKartu--;
       }
-      else {
-        System.out.println("Bandykite dar karta :(");
-        System.out.println(r);
-      }
-      kiekKartu--;
     }
   }
 }
